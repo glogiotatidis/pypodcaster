4468d49efe50b7878df8eb4985619a26ac352694
Josh Wheeler <mantlepro@gmail.com>
2015-09-23 23:05:18 -0700

    Working setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pypodcaster',
     version='0.7',
-    packages=['tests', 'pypodcaster'],
+    packages=find_packages(),
     url='http://github.com/mantlepro/pypodcaster',
     license='MIT',
     author='mantlepro',
     author_email='mantlepro@gmail.com',
-    description='Podcast feed generator'
+    description='Generate podcast feed from mp3 files',
+    package_data={'pypodcaster': ['templates/*']},
+    entry_points = {
+        'console_scripts': ['pypodcaster = pypodcaster.__main__:main']
+    }
 )
